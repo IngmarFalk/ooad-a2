a6ec1d63dc8ca7f54a6ba0bd9495358bf08f5a27
Ingmar Falk <if222gq@student.lnu.se>
2022-09-16 15:26:42 +0200

    Added cargo check to gitlab ci/cd yml

--- target/debug/deps/iana_time_zone-69e1f107d89017f4.d
+++ target/debug/deps/iana_time_zone-69e1f107d89017f4.d
@@ -1,6 +1,6 @@
-/Users/ingmarfalk/uni/OOAD_1DV607/a2ru/target/debug/deps/iana_time_zone-69e1f107d89017f4.rmeta: /Users/ingmarfalk/.cargo/registry/src/github.com-1ecc6299db9ec823/iana-time-zone-0.1.48/src/lib.rs /Users/ingmarfalk/.cargo/registry/src/github.com-1ecc6299db9ec823/iana-time-zone-0.1.48/src/tz_macos.rs
+/Users/ingmarfalk/uni/OOAD_1DV607/a2/target/debug/deps/iana_time_zone-69e1f107d89017f4.rmeta: /Users/ingmarfalk/.cargo/registry/src/github.com-1ecc6299db9ec823/iana-time-zone-0.1.48/src/lib.rs /Users/ingmarfalk/.cargo/registry/src/github.com-1ecc6299db9ec823/iana-time-zone-0.1.48/src/tz_macos.rs
 
-/Users/ingmarfalk/uni/OOAD_1DV607/a2ru/target/debug/deps/iana_time_zone-69e1f107d89017f4.d: /Users/ingmarfalk/.cargo/registry/src/github.com-1ecc6299db9ec823/iana-time-zone-0.1.48/src/lib.rs /Users/ingmarfalk/.cargo/registry/src/github.com-1ecc6299db9ec823/iana-time-zone-0.1.48/src/tz_macos.rs
+/Users/ingmarfalk/uni/OOAD_1DV607/a2/target/debug/deps/iana_time_zone-69e1f107d89017f4.d: /Users/ingmarfalk/.cargo/registry/src/github.com-1ecc6299db9ec823/iana-time-zone-0.1.48/src/lib.rs /Users/ingmarfalk/.cargo/registry/src/github.com-1ecc6299db9ec823/iana-time-zone-0.1.48/src/tz_macos.rs
 
 /Users/ingmarfalk/.cargo/registry/src/github.com-1ecc6299db9ec823/iana-time-zone-0.1.48/src/lib.rs:
 /Users/ingmarfalk/.cargo/registry/src/github.com-1ecc6299db9ec823/iana-time-zone-0.1.48/src/tz_macos.rs:
--- target/debug/deps/rand_chacha-26f2fc7aa3ac21c0.d
+++ target/debug/deps/rand_chacha-26f2fc7aa3ac21c0.d
@@ -1,6 +1,6 @@
-/Users/ingmarfalk/uni/OOAD_1DV607/a2ru/target/debug/deps/rand_chacha-26f2fc7aa3ac21c0.rmeta: /Users/ingmarfalk/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/lib.rs /Users/ingmarfalk/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/chacha.rs /Users/ingmarfalk/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/guts.rs
+/Users/ingmarfalk/uni/OOAD_1DV607/a2/target/debug/deps/rand_chacha-26f2fc7aa3ac21c0.rmeta: /Users/ingmarfalk/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/lib.rs /Users/ingmarfalk/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/chacha.rs /Users/ingmarfalk/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/guts.rs
 
-/Users/ingmarfalk/uni/OOAD_1DV607/a2ru/target/debug/deps/rand_chacha-26f2fc7aa3ac21c0.d: /Users/ingmarfalk/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/lib.rs /Users/ingmarfalk/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/chacha.rs /Users/ingmarfalk/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/guts.rs
+/Users/ingmarfalk/uni/OOAD_1DV607/a2/target/debug/deps/rand_chacha-26f2fc7aa3ac21c0.d: /Users/ingmarfalk/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/lib.rs /Users/ingmarfalk/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/chacha.rs /Users/ingmarfalk/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/guts.rs
 
 /Users/ingmarfalk/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/lib.rs:
 /Users/ingmarfalk/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/chacha.rs:
