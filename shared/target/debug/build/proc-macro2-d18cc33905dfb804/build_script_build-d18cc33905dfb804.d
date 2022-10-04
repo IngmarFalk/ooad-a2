5616bbf5072e7105ac0ecbb7673760df74106d29
Ingmar Falk <if222gq@student.lnu.se>
2022-10-04 14:12:05 +0200

    Removed shared target folder

--- shared/target/debug/build/proc-macro2-d18cc33905dfb804/build_script_build-d18cc33905dfb804.d
+++
@@ -1,5 +0,0 @@
-/Users/ingmarfalk/uni/OOAD_1DV607/a2/shared/target/debug/build/proc-macro2-d18cc33905dfb804/build_script_build-d18cc33905dfb804: /Users/ingmarfalk/.cargo/registry/src/github.com-1ecc6299db9ec823/proc-macro2-1.0.43/build.rs
-
-/Users/ingmarfalk/uni/OOAD_1DV607/a2/shared/target/debug/build/proc-macro2-d18cc33905dfb804/build_script_build-d18cc33905dfb804.d: /Users/ingmarfalk/.cargo/registry/src/github.com-1ecc6299db9ec823/proc-macro2-1.0.43/build.rs
-
-/Users/ingmarfalk/.cargo/registry/src/github.com-1ecc6299db9ec823/proc-macro2-1.0.43/build.rs:
--- shared/target/debug/build/syn-c7a28bf501e2f289/build_script_build-c7a28bf501e2f289.d
+++
@@ -1,5 +0,0 @@
-/Users/ingmarfalk/uni/OOAD_1DV607/a2/shared/target/debug/build/syn-c7a28bf501e2f289/build_script_build-c7a28bf501e2f289: /Users/ingmarfalk/.cargo/registry/src/github.com-1ecc6299db9ec823/syn-1.0.100/build.rs
-
-/Users/ingmarfalk/uni/OOAD_1DV607/a2/shared/target/debug/build/syn-c7a28bf501e2f289/build_script_build-c7a28bf501e2f289.d: /Users/ingmarfalk/.cargo/registry/src/github.com-1ecc6299db9ec823/syn-1.0.100/build.rs
-
-/Users/ingmarfalk/.cargo/registry/src/github.com-1ecc6299db9ec823/syn-1.0.100/build.rs:
